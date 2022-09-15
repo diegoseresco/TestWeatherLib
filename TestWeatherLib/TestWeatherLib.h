042972036c7c24a4990ab9028af102c45b8884e9
diego <92336377+diegosalcedov@users.noreply.github.com>
2022-09-14 21:32:47 -0500

    add framework

---
+++ TestWeatherLib/TestWeatherLib.h
@@ -0,0 +1,18 @@
+//
+//  TestWeatherLib.h
+//  TestWeatherLib
+//
+//  Created by diegitsen on 14/09/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestWeatherLib.
+FOUNDATION_EXPORT double TestWeatherLibVersionNumber;
+
+//! Project version string for TestWeatherLib.
+FOUNDATION_EXPORT const unsigned char TestWeatherLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestWeatherLib/PublicHeader.h>
+
+
---
+++ TestWeatherLib/WeatherUtils.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherUtils.swift
+//  TestWeatherLib
+//
+//  Created by diegitsen on 14/09/22.
+//
+
+import Foundation
+
+public struct WeatherUtils {
+    
+    public init() {}
+    
+    public func getTemperature() {
+        print("the temperature is 19 grades")
+    }
+    
+}
